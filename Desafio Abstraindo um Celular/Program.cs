55750d96ffa7a5a4b8228a767132963c3b5b33ca
Aylla Gomes <ayllabeatriz442@gmail.com>
2024-01-11 00:24:29 -0300

    Código Final

--- Desafio Abstraindo um Celular/Program.cs
+++ Desafio Abstraindo um Celular/Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+class Program {
+    static void Main() {
+        Nokia nokiaPhone = new Nokia("12345", "ModelX", "IMEI001", 32);
+        nokiaPhone.InstalarAplicativo("WhatsApp");
+
+        Iphone iphone = new Iphone("67890", "ModelY", "IMEI002", 64);
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
